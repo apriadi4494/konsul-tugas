31670c2bcf7bddc08d96155f1ee30c6e4f3ed2c9
Taryana <hatakekakasih2002@gmail.com>
2022-07-30 11:22:55 +0700

    feat(week1-tugasNomor3): added week1 taks number3

---
+++ nana/tugas3.js
@@ -0,0 +1,33 @@
+/* 
+    3.	Buatlah program yang memiliki satu variabel dengan nama “printSegitiga” yg berisi tipe data number yang menghasilkan output segitiga terbalik yang berisi angka
+*/
+
+
+let printTriangle = 6
+if(Number(printTriangle)){
+    let str = "";
+    for(let i = printTriangle; i > 0; i--){
+        for(let j = 1; j < i ; j++){
+            str += j;
+            str += " ";
+        }
+        str += "\n"
+    }
+    console.log(str)
+}else{
+    console.log("Data harus Number")
+}
+
+// let printTriangle = 6;
+
+// if(Number(printTriangle)){
+//     let str = "";
+//     for(let i = printTriangle; i > 0; i--){
+//         for(let j = i; j > 0; j--){
+//             str += j;
+//         }
+//         str += "\n"
+//     }
+//     console.log(str)
+// }
+
