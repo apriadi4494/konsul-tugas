bd09a938550bef064dfd93a33e88d7308c2183c1
salim-sarimu <abdussalimpro@gmail.com>
2022-07-30 21:43:00 +0700

    style(week1-soalNomor4): remove unnecessary inline spacing

--- salim/week1/tugasno4.js
+++ salim/week1/tugasno4.js
@@ -3,24 +3,24 @@
         // email: your email
         // hobby: your hobby
 const data = {
-    id        : 1,
-    name      : "Leanne Graham",
-    username  : "Bret",
-    email     : "Sincere@april.biz",
-    address   : {
-                    street   : "Kulas Light",
-                    suite    : "Apt. 556",
-                    city     : "Gwenborough",
-                    zipcode  : "92998-3874",
-                },
-    phone     : "1-770-736-8031 x56442",
-    website   : "hildegard.org",
+    id: 1,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.biz",
+    address:{
+                street: "Kulas Light",
+                suite: "Apt. 556",
+                city: "Gwenborough",
+                zipcode: "92998-3874",
+            },
+    phone: "1-770-736-8031 x56442",
+    website: "hildegard.org",
 };
 const dataMerge = {
     ...data,
-    name  : 'Abdus Salim',
-    hobby : 'learn to code',
-    email : 'abdussalimsan@gmail.com',
+    name: 'Abdus Salim',
+    hobby: 'learn to code',
+    email: 'abdussalimsan@gmail.com',
 };
 console.log(dataMerge);
 
