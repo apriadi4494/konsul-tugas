0a06ed2bf86efc82c60029dcf3ef3de26170f834
salim-sarimu <abdussalimpro@gmail.com>
2022-07-30 10:42:40 +0700

    fix(week1-soalNomor2): restore file tugasno2.js from last commit

--- salim/week1/tugasno2.js
+++ salim/week1/tugasno2.js
@@ -1,33 +1,36 @@
 
 
 function matapelajaran(mtk, bahasaIndonesia, bahasaInggris, ipa){
-   
     if(mtk == undefined || bahasaIndonesia == undefined || bahasaInggris == undefined || ipa == undefined)
     {
-        return console.log("lengkapi nilai")
+        console.log("lengkapi nilai");
     }
     else if(typeof(mtk) !== typeof(Number(mtk)) || typeof(bahasaIndonesia) !== typeof(Number(bahasaIndonesia)) || typeof(bahasaInggris) !== typeof(Number(bahasaInggris)) || typeof(ipa) !== typeof(Number(ipa)))
     {
-        return console.log("jangan memasukan selain angka")
+        console.log("jangan memasukan selain angka");
     }
-    else {
+    else{
         let average = (mtk + bahasaIndonesia + bahasaInggris + ipa) / 4;
-        console.log(`Rata-rata: ${average}`)
+        console.log(`Rata-rata: ${average}`);
         if(average >= 90 && average <= 100){
-            console.log(`Grade: A`)
-        }else if(average >= 80 && average <= 89){
-            console.log(`Grade: B`)
-        }else if(average >= 70 && average <= 79){
-            console.log(`Grade: C`)
-        }else if(average >= 60 && average <= 69){
-            console.log(`Grade: D`)
-        }else{
-            console.log(`Grade: E`)
+            console.log(`Grade: A`);
         }
+        else if(average >= 80 && average <= 89){
+            console.log(`Grade: B`);
+        }
+        else if(average >= 70 && average <= 79){
+            console.log(`Grade: C`);
+        }
+        else if(average >= 60 && average <= 69){
+           console.log(`Grade: D`);
+        }
+        else{
+            console.log(`Grade: E`);
+        } 
     }
 }
 
-matapelajaran(50, 58, 99, 100)
+matapelajaran(60,80,85,100);
 
 
 
