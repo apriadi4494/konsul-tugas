040ec63244cb34ce99292f95f4171a45378500ba
salim-sarimu <abdussalimpro@gmail.com>
2022-07-31 18:21:56 +0700

    style(week1-soalNomor2): remove some unnecessary lines

--- salim/week1/tugasno2.js
+++ salim/week1/tugasno2.js
@@ -1,5 +1,4 @@
 //the main function for checking, validating and printing the result exam 
-
 function mataPelajaranValidation(nilaiUNAS){
 
     const {mtk,bahasaIndonesia,bahasaInggris,ipa} = nilaiUNAS;
@@ -14,35 +13,31 @@ function mataPelajaranValidation(nilaiUNAS){
         console.log('Tolong hindari memasukkan selain angka');
         return;
     }
-
+    
     let result = mataPelajaranCalc(mtk, bahasaIndonesia, bahasaInggris, ipa)
     console.log(result);
-}
+};
 
 //the funtion for calculating the exam scores
-
 function mataPelajaranCalc(mtk, bahasaIndonesia, bahasaInggris, ipa){
     
     let average = (mtk + bahasaIndonesia + bahasaInggris + ipa) / 4;
 
-if(mtk < 0 || bahasaIndonesia < 0 || bahasaInggris <0 || ipa < 0 || mtk > 100 || bahasaIndonesia >100 || bahasaInggris >100 || ipa >100) return `Tolong masukkan setiap nilai hanya dalam interval 0 hingga 100 saja!`;
-
-if(average >= 90 && average <= 100) return `Rata-rata: ${average}\nGrade: A`;
-if(average >= 80 && average <= 89) return `Rata-rata: ${average}\nGrade: B`;
-if(average >= 70 && average <= 79) return `Rata-rata: ${average}\nGrade: C`;
-if(average >= 60 && average <= 69) return `Rata-rata: ${average}\nGrade: D`;
-return `Rata-rata: ${average}\nGrade: E`;     
-}
+    if(mtk < 0 || bahasaIndonesia < 0 || bahasaInggris <0 || ipa < 0 || mtk > 100 || bahasaIndonesia >100 || bahasaInggris >100 || ipa >100) return `Tolong masukkan setiap nilai hanya dalam interval 0 hingga 100 saja!`;
+    if(average >= 90 && average <= 100) return `Rata-rata: ${average}\nGrade: A`;
+    if(average >= 80 && average <= 89) return `Rata-rata: ${average}\nGrade: B`;
+    if(average >= 70 && average <= 79) return `Rata-rata: ${average}\nGrade: C`;
+    if(average >= 60 && average <= 69) return `Rata-rata: ${average}\nGrade: D`;
+    return `Rata-rata: ${average}\nGrade: E`;     
+};
 
 //the object that act as a parameter receiver
-
 const nilaiUn= {
-    mtk: 100,
-    bahasaIndonesia: 60,
+    mtk: 30,
+    bahasaIndonesia: ,
     bahasaInggris: 90,
     ipa: 100
-}
+};
 
 //the callback for main function 
-
 mataPelajaranValidation(nilaiUn);
