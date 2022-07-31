135654b9ba85a7cff3b2b26f46a9a59fc6fb335c
salim-sarimu <abdussalimpro@gmail.com>
2022-07-31 18:11:40 +0700

    style(week1-soalNomor2): fix the code for output typing at mataPelajaranCalc's function

--- salim/week1/tugasno2.js
+++ salim/week1/tugasno2.js
@@ -27,16 +27,11 @@ function mataPelajaranCalc(mtk, bahasaIndonesia, bahasaInggris, ipa){
 
 if(mtk < 0 || bahasaIndonesia < 0 || bahasaInggris <0 || ipa < 0 || mtk > 100 || bahasaIndonesia >100 || bahasaInggris >100 || ipa >100) return `Tolong masukkan setiap nilai hanya dalam interval 0 hingga 100 saja!`;
 
-if(average >= 90 && average <= 100) return `Rata-rata: ${average}
-Grade: A`;
-if(average >= 80 && average <= 89) return `Rata-rata: ${average}
-Grade: B`;
-if(average >= 70 && average <= 79) return `Rata-rata: ${average}
-Grade: C`;
-if(average >= 60 && average <= 69) return `Rata-rata: ${average}
-Grade: D`;
-return `Rata-rata: ${average}
-Grade: E`;     
+if(average >= 90 && average <= 100) return `Rata-rata: ${average}\nGrade: A`;
+if(average >= 80 && average <= 89) return `Rata-rata: ${average}\nGrade: B`;
+if(average >= 70 && average <= 79) return `Rata-rata: ${average}\nGrade: C`;
+if(average >= 60 && average <= 69) return `Rata-rata: ${average}\nGrade: D`;
+return `Rata-rata: ${average}\nGrade: E`;     
 }
 
 //the object that act as a parameter receiver
