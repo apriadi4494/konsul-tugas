fa9974260081ea203ade3bc6a34dca9c7ce20586
Rifqi Ahmad Pratama <rifqiahmad234a@gmail.com>
2022-08-04 13:09:21 +0700

    feat(week2-soalno1):Added task week 2 number 1

---
+++ ahmad/week 2/no1.js
@@ -0,0 +1,62 @@
+// // 1
+//toUpperCase
+// let nama = "Rifqi Ahmad Pratama";
+// let cekNama = nama.toUpperCase();
+// console.log(cekNama);
+// // // 2
+// // toLowerCase
+// let nama1 = "Rifqi Ahmad Pratama";
+// let cekNama1 = nama1.toLowerCase();
+// console.log(cekNama1);
+
+// // 3
+// // pop
+// const myNum = [1, 2, 3, 4, 5, 7, 5, 2];
+// myNum.pop();
+// console.log(myNum);
+
+// 4
+// // push
+// const numberPush = [1, 2, 3, 4, 5];
+// numberPush.push("hahahah");
+// console.log(numberPush);
+
+// // 5
+// // // match
+// let x = "Kamu sedang di jalanan";
+// let y = "tidur";
+// let xy = x.match(y);
+// console.log(`Hasil Data .match() adalah ${xy}`);
+
+// // 6
+// // slice
+// var arr1 = [1, 2, 3, 4, "a", "b", "c", "d"];
+// var arr2 = arr1.slice(3);
+// console.log(arr2.toString());
+
+// // 7
+// var arr1 = [];
+// // unshift
+// arr1.unshift("a", "b", "c");
+// console.log(arr1.toString());
+
+// // 8
+// replace()
+// let x = "jambu dan alpukat dibikin jus";
+// let y = "jambu";
+// // y sebagai data lama
+// let z = "jeruk";
+// // z data baru
+// let xyz = x.replace(y, z);
+// console.log(xyz);
+
+// 9
+// // indexOf
+// let x = "jambu dan alpukat dibikin jus";
+// let xy = x.indexOf("u");
+// console.log(xy);
+
+// 10
+// let a1 = "Kami sedang makan di rumah";
+// let a2 = a1.length;
+// console.log(`Panjang karakter adalah ${a2}`);
